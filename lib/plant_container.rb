e0b12c11e6c6fdc9f015ad6ed2b81c938b0d41e5
Ian Knox <knoxid@gmail.com>
2014-09-23 09:34:15 -0600

    returns avgs

--- lib/plant_container.rb
+++ lib/plant_container.rb
@@ -1,5 +1,27 @@
+require "csv"
+
 class PlantContainer
   def initialize(file)
     @file = file
+  end
+
+  def averages(container)
+    ph_sum, nsl_sum, temp_sum, water_level_sum, count = 0, 0, 0, 0, 0.0
+    CSV.foreach(@file, {:col_sep => " "}) do |row|
+      if row[3][-1].to_i == container
+        ph_sum += row[4].to_f
+        nsl_sum += row[5].to_i
+        temp_sum += row[6].to_i
+        water_level_sum += row[7].to_f
+        count += 1.0
+      end
+    end
+    {
+      container: container,
+      ph: (ph_sum/count).round(2),
+      nsl: (nsl_sum/count).round(2),
+      temp: (temp_sum/count).round(2),
+      water_level: (water_level_sum/count).round(2)
+    }
   end
 end
--- spec/plant_container_spec.rb
+++ spec/plant_container_spec.rb
@@ -4,4 +4,18 @@ describe "Plant Container" do
   it "takes a file path as argument" do
     PlantContainer.new("./data/metrics.tsv")
   end
+
+  it "returns a hash with averages" do
+    container = PlantContainer.new("./data/metrics.tsv")
+    averages = container.averages(1)
+    expected_output = {
+      container: 1,
+      ph: 5.01,
+      nsl: 39.02,
+      temp: 57.76,
+      water_level: 2.12
+    }
+
+    expect(averages).to eq(expected_output)
+  end
 end
